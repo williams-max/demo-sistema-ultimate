f77d05cbb2a033041c69d3539840282b51ae71fd
Israel <almanzaisrael75@gmail.com>
2023-04-24 09:47:43 -0400

    mui styles 3

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   
  
       </div>
-      <h1>Demo Sistema Dates </h1>
+      <h1>Demo Sistema </h1>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer
         components={[
